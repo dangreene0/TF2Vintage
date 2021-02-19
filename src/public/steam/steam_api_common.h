574fb71c38589402ae3bef7510dc07a1edb357eb
Deathreus <csmoore96@yahoo.com>
2021-02-19 14:03:52 -0500

    Update steam_api_common.h

--- src/public/steam/steam_api_common.h
+++ src/public/steam/steam_api_common.h
@@ -208,6 +208,8 @@ public:
 	void Register( T *pObj, func_t func );
 	void Unregister();
 
+	void operator() ( P *pParam ) { Run( (void *)pParam ); }
+
 protected:
 	virtual void Run( void *pvParam );
 	
