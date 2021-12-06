677c4f0144fc45fee4876b5c103099392ae86c13
Deathreus <csmoore96@yahoo.com>
2021-12-05 22:32:02 -0500

    Update gh-cache.sh

--- .github/gh-cache.sh
+++ .github/gh-cache.sh
@@ -3,14 +3,23 @@ lib_dir=src/lib/public
 lib_ext=.lib
 dll_ext=.dll
 dbg_ext=.pdb
-if [ 'uname' = "Linux" ]; then
+if [ `uname` = "Linux" ]; then
     lib_dir=src/lib/public/linux32
     lib_ext=.a
     dll_ext=.so
     dbg_ext=.dbg
 fi
 
-if [ "$1" != "restore" ]; then
+RESTORE=0
+if [ $# -eq 1 ]; then
+    if [ $1 = 'restore' ]; then
+        RESTORE=1
+    else
+        echo "Bad arguments passed to script (${1})."
+    fi
+fi
+
+if [ $RESTORE -eq 1 ]; then
     mkdir -p src/ccache
     mv -f game/tf2vintage/bin/* src/ccache
     mv -f $lib_dir/mathlib$lib_ext $lib_dir/tier1$lib_ext $lib_dir/raytrace$lib_ext $lib_dir/vgui_controls$lib_ext src/ccache
