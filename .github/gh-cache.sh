373ff864bf0977e3fecdba4c5e5f0aecf7b8f8d9
Deathreus <csmoore96@yahoo.com>
2021-12-05 23:47:35 -0500

    Fix script

--- .github/gh-cache.sh
+++ .github/gh-cache.sh
@@ -20,10 +20,10 @@ if [ $# -eq 1 ]; then
 fi
 
 if [ $RESTORE -eq 1 ]; then
+    mv -f src/ccache/*$dll_ext src/ccache/*$dbg_ext game/tf2vintage/bin
+    mv -f src/ccache/*$lib_ext $lib_dir
+else
     mkdir -p src/ccache
     mv -f game/tf2vintage/bin/* src/ccache
     mv -f $lib_dir/mathlib$lib_ext $lib_dir/tier1$lib_ext $lib_dir/raytrace$lib_ext $lib_dir/vgui_controls$lib_ext src/ccache
-else
-    mv -f src/ccache/*$dll_ext src/ccache/*$dbg_ext game/tf2vintage/bin
-    mv -f src/ccache/*$lib_ext $lib_dir
 fi
