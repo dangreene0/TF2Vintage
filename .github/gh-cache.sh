7384447a4bb1d0a641de7048cbc1efc9eda56eb3
Deathreus <csmoore96@yahoo.com>
2021-12-06 20:23:47 -0500

    Update gh-cache.sh

--- .github/gh-cache.sh
+++ .github/gh-cache.sh
@@ -3,7 +3,7 @@ lib_dir=src/lib/public
 lib_ext=.lib
 dll_ext=.dll
 dbg_ext=.pdb
-if [ `uname` = "Linux" ]; then
+if [ `uname` = 'Linux' ]; then
     lib_dir=src/lib/public/linux32
     lib_ext=.a
     dll_ext=.so
@@ -23,7 +23,6 @@ if [ $RESTORE -eq 1 ]; then
     mv -f src/ccache/*$dll_ext src/ccache/*$dbg_ext game/tf2vintage/bin
     mv -f src/ccache/*$lib_ext $lib_dir
 else
-    mkdir -p src/ccache
     mv -f game/tf2vintage/bin/* src/ccache
     mv -f $lib_dir/mathlib$lib_ext $lib_dir/tier1$lib_ext $lib_dir/raytrace$lib_ext $lib_dir/vgui_controls$lib_ext src/ccache
 fi
