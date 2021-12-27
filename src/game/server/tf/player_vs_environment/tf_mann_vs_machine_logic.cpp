bb457bc4b9da6627c1951f67a63a20728484beec
Deathreus <csmoore96@yahoo.com>
2021-12-26 20:54:23 -0500

    Update tf_mann_vs_machine_logic.cpp

--- src/game/server/tf/player_vs_environment/tf_mann_vs_machine_logic.cpp
+++ src/game/server/tf/player_vs_environment/tf_mann_vs_machine_logic.cpp
@@ -126,7 +126,60 @@ void CMannVsMachineLogic::Update( void )
 //-----------------------------------------------------------------------------
 void CMannVsMachineLogic::InitPopulationManager(void)
 {
+	bool bFound = false;
+	char szFileName[MAX_PATH] ={0};
+
+	if ( TFGameRules()->GetNextMvMPopfile()[0] != '\0' )
+	{
+		Q_snprintf( szFileName, sizeof( szFileName ), "scripts/population/%s.pop", TFGameRules()->GetNextMvMPopfile() );
+		if ( g_pFullFileSystem->FileExists( szFileName, "GAME" ) )
+		{
+			bFound = true;
+		}
+		else
+		{
+			// It might need the additional map prefix to find the file
+			Q_snprintf( szFileName, sizeof( szFileName ), "scripts/population/%s_%s.pop", STRING( gpGlobals->mapname ), TFGameRules()->GetNextMvMPopfile() );
+			if ( g_pFullFileSystem->FileExists( szFileName, "GAME" ) )
+				bFound = true;
+			
+			if ( !bFound )
+				Warning( "Population file '%s' not found", TFGameRules()->GetNextMvMPopfile() );
+		}
+	}
+
+	// Fallback to defaults
+	if ( !bFound )
+	{
+		CUtlVector<CUtlString> defaultPopFileList;
+		g_pPopulationManager->FindDefaultPopulationFileShortNames( defaultPopFileList );
+		if ( !defaultPopFileList.IsEmpty() )
+		{
+			CUtlString defaultPopFileName;
+			if ( g_pPopulationManager->FindPopulationFileByShortName( defaultPopFileList[0], &defaultPopFileName ) )
+			{
+				V_strncpy( szFileName, defaultPopFileName.Get(), sizeof( szFileName ) );
+				bFound = true;
+			}
+		}
+	}
+
+	if ( !bFound )
+		Q_snprintf( szFileName, sizeof( szFileName ), "scripts/population/%s.pop", STRING( gpGlobals->mapname ) );
+
 	// TODO: This is a bit weird, they check that m_populationManager is null and if so, create one
 	// but they also use g_pPopulationManager and never validate it
+	if ( m_populationManager && !FStrEq( m_populationManager->GetPopulationFilename(), szFileName ) )
+	{
+		UTIL_RemoveImmediate( m_populationManager );
+		m_populationManager = NULL;
+	}
+
+	if ( !m_populationManager )
+	{
+		m_populationManager = (CPopulationManager *)CreateEntityByName( "info_populator" );
+		m_populationManager->SetPopulationFilename( szFileName );
+	}
+
 	TFGameRules()->SetNextMvMPopfile( "" );
 }
