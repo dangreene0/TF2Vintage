dangreene0/TF2Vintage

commit fbb4f8bee25af53c1cfe462b5c238b61bff17e65

[Test] Update workflows

.github/workflows/linux.yml
@@ -1,12 +1,7 @@
 name: Linux
 
 on: 
-  push:
-    paths:
-      - 'src/**'
-  pull_request:
-   paths:
-      - 'src/**'
+  workflow_call:
 
 env:
   STEAM_RUNTIME_PATH: /valve/steam-runtime
@@ -74,14 +69,9 @@ jobs:
 
       - name: Build project
         run: cd src && sudo make -f TF2vintage.mak
-      
-      - name: Pack
-        run: |
-          sudo tar cJfv Linux.tar.xz game/tf2vintage/bin
-          sudo bash .github/gh-cache.sh
 
       - name: Upload product
         uses: actions/upload-artifact@v2
         with:
-          name: TF2Vintage
-          path: Linux.tar.xz
+          name: tf2vintage-linux
+          path: game/tf2vintage/bin/

.github/workflows/release.yml
@@ -1,28 +1,59 @@
 name: Release
 
-on:
+on: 
   push:
-  release:
-    types: [ created ]
+    paths:
+      - 'src/**'
+      - 'game/tf2vintage/**'
+  pull_request:
+   paths:
+      - 'src/**'
+      - 'game/tf2vintage/**'
+  workflow_dispatch:
+
+env:
+  STEAM_RUNTIME_PATH: /valve/steam-runtime
+  CCACHE_VERSION: 4.2.1
+  CCACHE_DIR: ${{ github.workspace }}/src/ccache
 
 jobs:
-  release:
-    if: startsWith( github.ref, 'refs/tags' )
-    runs-on: ubuntu-latest
+  build-windows:
+    uses: ./.github/workflows/windows.yml
 
+  build-linux:
+    uses: ./.github/workflows/linux.yml
+
+
+  build-game:
+    needs: [build-windows, build-linux]
+    runs-on:
+      ubuntu-20.04
+    
     steps:
-      - name: Download artifact
-        uses: actions/download-artifact@v2
+      - name: Checkout game assets
+        uses: actions/checkout@v2
+      - name: Download Windows Binary
+        uses: actions/download-artifact@v3
         with:
-          name: TF2Vintage
-        
-      - name: Create release
-        id: create_release
-        uses: actions/create-release@v1
-        env:
-          GITHUB_TOKEN: ${{secrets.GITHUB_TOKEN}}
+          name: tf2vintage-windows
+          path: game/tf2vintage/bin/
+      - name: Download Linux Binary
+        uses: actions/download-artifact@v3
         with:
-          tag_name: ${{github.ref}}
-          release_name: ${{github.ref}}
-          draft: false
-          prerelease: false
+          name: tf2vintage-linux
+          path: game/tf2vintage/bin/
+      - name: Pack [7z]
+        uses: edgarrc/action-7z@v1
+        with:
+          args: 7z a -t7z -mx=9 tf2vintage.7z ./game/tf2vintage/
+      - name: Upload product [7z]
+        uses: actions/upload-artifact@v3
+        with:
+          name: tf2vintage
+          path: tf2vintage.7z
+      - name: Clean up leftover binaries
+        uses: geekyeggo/delete-artifact@v1
+        with:
+          name: |
+            tf2vintage-windows
+            tf2vintage-linux

.github/workflows/tf2vintage.yml
@@ -1,148 +1,0 @@
-name: tf2vintage
-
-on: 
-  push:
-    paths:
-      - 'src/**'
-      - 'game/tf2vintage/**'
-  pull_request:
-   paths:
-      - 'src/**'
-      - 'game/tf2vintage/**'
-
-env:
-  STEAM_RUNTIME_PATH: /valve/steam-runtime
-  CCACHE_VERSION: 4.2.1
-  CCACHE_DIR: ${{ github.workspace }}/src/ccache
-
-jobs:
-  build-windows:
-    runs-on:
-      windows-2019
-    
-    steps:
-      - name: Checkout [Windows]
-        uses: actions/checkout@v2
-
-      - name: Configure dependencies [Windows]
-        uses: ilammy/msvc-dev-cmd@v1
-
-      - name: Configure registry [Windows]
-        shell: powershell
-        run: |
-          REG ADD "HKLM\Software\Microsoft\VisualStudio\12.0\Projects\{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}" /v "DefaultProjectExtension" /t REG_SZ  /d "vcxproj" /f /reg:32
-          REG ADD "HKLM\Software\Microsoft\VisualStudio\12.0\Projects\{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}" /v "PossibleProjectExtensions" /t REG_SZ /d "vcxproj;vcxitems;vcproj;mak" /f /reg:32
-
-      - name: Create project [Windows]
-        run: cd src;devtools/bin/vpc.exe /tf2vintage /2015 +game /mksln TF2vintage.sln
-
-      - name: Build project [Windows]
-        run: cd src;devenv TF2vintage.sln /Build Release
-
-      - name: Store Windows Binaries
-        uses: actions/upload-artifact@v3
-        with:
-          name: tf2vintage-windows
-          path: game/tf2vintage/bin/
-
-  build-linux:
-    runs-on:
-      ubuntu-20.04
-    
-    steps:
-      - name: Checkout [Linux]
-        uses: actions/checkout@v2
-
-      - id: get-date
-        run: |
-          echo "::set-output name=date::$(/bin/date -u "+%Y%m%d")"
-      
-      - uses: actions/cache@v2
-        id: cache
-        with:
-          path: |
-            ${{ env.CCACHE_DIR }}
-          key: ${{ runner.os }}-${{ steps.get-date.outputs.date }}
-          restore-keys: ${{ runner.os }}-
-      
-      - name: Download ccache [Linux]
-        id: ccache
-        shell: cmake -P {0}
-        run: |
-          set(ccache_url "https://github.com/cristianadam/ccache/releases/download/v$ENV{CCACHE_VERSION}/${{ runner.os }}.tar.xz")
-          file(DOWNLOAD "${ccache_url}" ./ccache.tar.xz SHOW_PROGRESS)
-          execute_process(COMMAND ${CMAKE_COMMAND} -E tar xvf ./ccache.tar.xz)
-          file(COPY ./ccache DESTINATION src/devtools/bin/linux USE_SOURCE_PERMISSIONS)
-      
-      - name: Setup ccache [Linux]
-        run: |
-          sudo mkdir -p game/tf2vintage/bin
-          cd src
-          sudo mkdir -p ./ccache/tmp
-          cd devtools/bin/linux
-          sudo ./ccache --set-config cache_dir=$CCACHE_DIR
-          sudo ./ccache --set-config compression_level=6
-      
-      - name: Restore cache [Linux]
-        if: steps.cache.outputs.cache-hit == 'true'
-        run: sudo bash .github/gh-cache.sh restore
-      
-      - name: Configure dependencies [Linux]
-        run: sudo apt update && sudo apt install -y build-essential gcc-multilib g++-multilib
-
-      - name: Create project [Linux]
-        run: cd src && sudo devtools/bin/vpc /tf2vintage +game /mksln TF2vintage
-      
-      - name: Download Steam runtime [Linux]
-        run: |
-          cd /
-          sudo mkdir valve
-          cd valve
-          sudo wget http://media.steampowered.com/client/runtime/steam-runtime-sdk_latest.tar.xz
-          sudo tar xvf steam-runtime-sdk_latest.tar.xz
-          sudo mv steam-runtime-sdk_2013-09-05 steam-runtime
-          sudo bash $STEAM_RUNTIME_PATH/setup.sh --release --target="i386 amd64" --auto-upgrade
-
-      - name: Build project [Linux]
-        run: cd src && sudo make -f TF2vintage.mak
-
-      - name: Store Linux Binaries
-        uses: actions/upload-artifact@v3
-        with:
-          name: tf2vintage-linux
-          path: game/tf2vintage/bin/
-
-
-  build-game:
-    needs: [build-windows, build-linux]
-    runs-on:
-      ubuntu-20.04
-    
-    steps:
-      - name: Checkout game assets
-        uses: actions/checkout@v2
-      - name: Download Windows Binary
-        uses: actions/download-artifact@v3
-        with:
-          name: tf2vintage-windows
-          path: game/tf2vintage/bin/
-      - name: Download Linux Binary
-        uses: actions/download-artifact@v3
-        with:
-          name: tf2vintage-linux
-          path: game/tf2vintage/bin/
-      - name: Pack [7z]
-        uses: edgarrc/action-7z@v1
-        with:
-          args: 7z a -t7z -mx=9 tf2vintage.7z ./game/tf2vintage/
-      - name: Upload product [7z]
-        uses: actions/upload-artifact@v3
-        with:
-          name: tf2vintage
-          path: tf2vintage.7z
-      - name: Clean up leftover binaries
-        uses: geekyeggo/delete-artifact@v1
-        with:
-          name: |
-            tf2vintage-windows
-            tf2vintage-linux

.github/workflows/windows.yml
@@ -1,12 +1,7 @@
 name: Windows
 
 on: 
-  push:
-    paths:
-      - 'src/**'
-  pull_request:
-   paths:
-      - 'src/**'
+  workflow_call:
 
 jobs:
   build:
@@ -32,12 +27,8 @@ jobs:
       - name: Build project
         run: cd src;devenv TF2vintage.sln /Build Release
 
-      - name: Pack
-        shell: bash
-        run: tar cJfv Windows.tar.xz game/tf2vintage/bin
-
       - name: Upload product
         uses: actions/upload-artifact@v2
         with:
-          name: TF2Vintage
-          path: Windows.tar.xz
+          name: tf2vintage-windows
+          path: game/tf2vintage/bin/

commit 3b20323c91da0ac68bea6566afe5cbf5679975e3

Fix Linux workflow
Commit fbb4f8bee25af53c1cfe462b5c238b61bff17e65 broke it, oops

.github/gh-cache.sh
@@ -19,7 +19,6 @@ if [ $# -eq 1 ]; then
     fi
 fi
 
-cd ..
 if [ $RESTORE -eq 1 ]; then
     mv -f src/ccache/*$dll_ext src/ccache/*$dbg_ext game/tf2vintage/bin
     mv -f src/ccache/*$lib_ext $lib_dir

.github/workflows/linux.yml
@@ -75,3 +75,6 @@ jobs:
         with:
           name: tf2vintage-linux
           path: game/tf2vintage/bin/
+      
+      - name: Move to cache
+        run: sudo bash .github/gh-cache.sh