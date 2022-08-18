4769228b226e651257ef28157033d46cbba69da0
Deathreus <csmoore96@yahoo.com>
2022-08-17 22:13:54 -0400

    Update steam_api.h

--- src/public/steam/steam_api.h
+++ src/public/steam/steam_api.h
@@ -43,9 +43,6 @@
 #include "isteamparentalsettings.h"
 #include "isteaminput.h"
 #include "isteamremoteplay.h"
-#include "isteamnetworkingmessages.h"
-#include "isteamnetworkingsockets.h"
-#include "isteamnetworkingutils.h"
 
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------//
